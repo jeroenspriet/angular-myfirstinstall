65f82bd381eaf8392beda47bc1b262d75ef583bd
jeroenspriet <jeroen.spriet@mediahuis.be>
2019-12-30 13:54:10 +0100

    firstinstall

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component_org.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-install';
+  name = 'Jeroen';
+}
